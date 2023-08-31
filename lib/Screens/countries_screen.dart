d9234d5fdc62a07b982fe8ff5252539271d5bc4f
unknown <mohamedabdelhamed555>
2023-08-31 03:17:28 +0300

    leagues

--- lib/Screens/countries_screen.dart
+++ lib/Screens/countries_screen.dart
@@ -1,3 +1,4 @@
+import 'package:final_project/Screens/leagues_screen.dart';
 import 'package:flutter/material.dart';
 
 class GridItem {
@@ -67,27 +68,6 @@ class _CountriesState extends State<Countries> {
             ),
           );
         },
-      ),
-    );
-  }
-}
-
-class LeaguesScreen extends StatelessWidget {
-  final String country;
-
-  LeaguesScreen({required this.country});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Leagues of $country'),
-      ),
-      body: Center(
-        child: Text(
-          'Display leagues of $country',
-          style: TextStyle(fontSize: 20),
-        ),
       ),
     );
   }
