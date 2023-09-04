dc4e81322b92e9867376201388867ea98b54cdbe
unknown <mohamedabdelhamed555>
2023-09-05 02:35:29 +0300

    api

--- lib/Screens/leagues_screen.dart
+++ lib/Screens/leagues_screen.dart
@@ -65,3 +65,41 @@ class LeaguesScreen extends StatelessWidget {
     );
   }
 }
+
+
+
+/* import 'package:final_project/Widget/teams_screen_view_body.dart';
+import 'package:flutter/material.dart';
+
+class LeaguesScreen extends StatelessWidget {
+  final String country;
+
+  LeaguesScreen({required this.country});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xff14142B),
+      appBar: AppBar(
+        title: Text('Leagues of $country'),
+        backgroundColor: Color(0xff14142B),
+      ),
+      body: Center(
+        child: TextButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute<void>(
+                builder: (BuildContext context) => const TeamsScreenViewBody(),
+              ),
+            );
+          },
+          child: Text(
+            'Display leagues of $country',
+            style: TextStyle(fontSize: 20),
+          ),
+        ),
+      ),
+    );
+  }
+} */
